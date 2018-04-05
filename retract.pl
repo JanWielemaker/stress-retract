fcaa5bba9c988c2c25e552dfa265500c22563d5c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-04-05 23:56:20 +0200

    Create the dynamic predicate while reading it.

--- retract.pl
+++ retract.pl
@@ -2,18 +2,22 @@
 :- use_module(library(apply)).
 
 :- dynamic
-	a/1.
+	a/1,
+	complete/0.
 
 test(Threads, Length) :-
 	numlist(0, Length, List),
 	sum_list(List, Sum),
-	forall(between(0, Length, X),
-	       ( term(X, T),
-		 assertz(a(T)))),
 	thread_self(Me),
 	length(TIDS, Threads),
 	maplist(thread_create(collect(Me, Length)), TIDS),
-	maplist(thread_join, TIDS),
+	forall(between(0, Length, X),
+	       ( term(X, T),
+		 assertz(a(T)))),
+	setup_call_cleanup(
+	    asserta(complete),
+	    maplist(thread_join, TIDS),
+	    retractall(complete)),
 	findall(C, ( between(1, Threads, _),
 		     thread_get_message(collected(C))
 		   ), CL),
@@ -25,10 +29,17 @@ test(Threads, Length) :-
 	).
 
 collect(Main, Length) :-
-	collect(0, Length, 0, N),
+	collect(Length, 0, N),
 	thread_send_message(Main, collected(N)).
 
-collect(I, End, N0, N) :-
+collect(Length, N0, N) :-
+	collect1(0, Length, N0, N1),
+	(   complete
+	->  collect1(0, Length, N1, N)
+	;   collect(Length, N1, N)
+	).
+
+collect1(I, End, N0, N) :-
 	I =< End, !,
 	(   term(I, T),
 	    retract(a(T))
@@ -36,8 +47,8 @@ collect(I, End, N0, N) :-
 	;   N1 = N0
 	),
 	I2 is I+1,
-	collect(I2, End, N1, N).
-collect(_, _, N, N).
+	collect1(I2, End, N1, N).
+collect1(_, _, N, N).
 
 term(N, x(N)) :- N mod  5 =:= 0, !.
 term(N, y(N)) :- N mod 13 =:= 0, !.
